72014303e862e3f070290276b928e5708e4dd9da
laihuamin <1445509994@qq.com>
2017-08-20 10:39:38 +0800

    骨架

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var swiper = new Swiper('.swiper-container', {
+        autoplay: 5000,
+        dirction: 'vertial',
+        loop: true,
+        nextButton: '.swiper-button-prev'
+    })
+});
