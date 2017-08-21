88b49a4f93173aad0718ffa0a0ac2035ff8ca42e
laihuamin <1445509994@qq.com>
2017-08-21 21:56:34 +0800

    8.21

--- demo1/js/index.js
+++ demo1/js/index.js
@@ -2,7 +2,14 @@ $(document).ready(function(){
     var swiper = new Swiper('.swiper-container', {
         direction: 'vertical',
         loop: true,
-        nextButton: '.btn'
+        nextButton: '.btn',
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素 
+            swiperAnimate(swiper); //初始化完成开始动画
+        }, 
+        nSlideChangeEnd: function(swiper){ 
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }       
     })
     //home首页
     function home(data){
@@ -18,12 +25,21 @@ $(document).ready(function(){
         }
         $('.header-con').text(staticData.title);
         $('.people-num').text(staticData.people);
-        const block = '<span class="block"></span>';
+        const block = '<div class="block flash animated"></div>';
+        
         const result = [];
         staticData.data.forEach((item) => {
-            let temp = Math.floor(item / 500);
+            let resultTemp = `${item}`;   
+            let temp = Math.floor(item / 5000);
+            for(let i = 0; i < temp; i++){
+                resultTemp += block;
+            }
+            result.push(resultTemp);
         });
-        lastDataHtml = `<div class="last-one"></div>`
+        $('.last-one').html(result[0]);
+        $('.last-two').html(result[1]);
+        $('.now-one').html(result[2]);
+        $('.now-two').html(result[3]);
     }
     //格式化数据
     function init(data){
