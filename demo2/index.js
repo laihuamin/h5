ebb1180fda4211c3e27a44c9591e35bb9b9bec3a
laihuamin <1445509994@qq.com>
2017-11-05 22:07:29 +0800

    position-finish

--- demo2/index.js
+++ demo2/index.js
@@ -4,6 +4,8 @@ function Radar() {
     this.deg = [];
     this.rad = [];
     this.imgGroup = [];
+    this.top = [];
+    this.left = [];
 }
 //入口函数
 Radar.prototype.init = function() {
@@ -30,12 +32,32 @@ Radar.prototype.imgSize = function() {
     //在0到600之间随意取半径
     let rad = Math.round(Math.random() * 600);
     this.rad.push(rad);
+    //随机取top
+    let top = Math.round(Math.random() * 100);
+    this.top.push(top);
+    //随机取left
+    let left = Math.round(Math.random() * 100);
+    this.left.push(left);
 }
 //给那个item添加class
 Radar.prototype.prodClass = function() {
     for(let i = 0; i < this.imgGroup.length; i++) {
         let className = 'img-' + i;
         this.imgGroup[i].classList.add(className);
+    }
+}
+//给每个class添加属性
+Radar.prototype.setClass = function() {
+    for(let i = 0; i < this.imgGroup.length; i++) {
+        this.imgGroup[i].style = {
+            width: `${this.width[i]}px`,
+            height: `${this.height[i]}px`,
+            top: 0;
+            left: 50%;
+            transform: translate3d(-50%, -50%, 0);
+            animation: img-big-fourth 6s infinite linear;
+            animation-delay: 3s;
+        }
     }
 }
 
